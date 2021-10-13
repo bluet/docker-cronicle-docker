e6e1f99c252515b7acb52bf000a88c711f1f0ba5
BlueT - Matthew Lien - 練喆明 <bluet@bluet.org>
2021-10-13 12:46:43 +0800

    update comment in docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -4,10 +4,14 @@ VERSION=0.8.62
 
 docker build -t bluet/cronicle-docker .
 docker scan bluet/cronicle-docker:latest
+
 #docker tag bluet/cronicle-docker:latest bluet/cronicle-docker:0.8.62
 #git tag "0.8.62" -a -m "docker 20.10, cronicle 0.8.62"
 #git push --tags
+
+# Fixes busybox trigger error https://github.com/tonistiigi/xx/issues/36#issuecomment-926876468
 # docker run --privileged -it --rm tonistiigi/binfmt --install all
+
 # docker buildx create --use
 
 while true; do
