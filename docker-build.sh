38521d417a507a5b1cc25bc1b3e41f9f2b8c3375
BlueT - Matthew Lien - 練喆明 <bluet@bluet.org>
2024-10-20 00:53:13 +0800

    use docker cloud build

--- docker-build.sh
+++ docker-build.sh
@@ -14,14 +14,14 @@ docker tag bluet/cronicle-docker:latest bluet/cronicle-docker:${VERSION}
 #git push --tags
 
 # Fixes busybox trigger error https://github.com/tonistiigi/xx/issues/36#issuecomment-926876468
-docker run --privileged -it --rm tonistiigi/binfmt --install all
+#docker run --privileged -it --rm tonistiigi/binfmt --install all
 
-docker buildx create --use
+#docker buildx create --use
 
 while true; do
         read -p "Is VERSION=${VERSION} the current latest version? (We're going to build multi-platform images and push) [y/N]" yn
         case $yn in
-                [Yy]* ) docker buildx build -t bluet/cronicle-docker:latest -t bluet/cronicle-docker:${VERSION} --platform linux/amd64,linux/arm64/v8 --pull --push .; break;;
+                [Yy]* ) docker buildx buildi --builder cloud-bluet-test -t bluet/cronicle-docker:latest -t bluet/cronicle-docker:${VERSION} --platform linux/amd64,linux/arm64/v8 --pull --push .; break;;
                 [Nn]* ) break;;
                 * ) echo "";;
         esac
